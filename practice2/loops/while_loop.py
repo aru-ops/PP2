d08c0cfdb6428510ea61f094a5c035896248a252
aru-ops <aruzantaboldieva@gmail.com>
2026-02-04 21:54:35 +0500

    while loop

---
+++ practice2/loops/while_loop.py
@@ -0,0 +1,11 @@
+# Example 1: Basic while loop
+i = 1
+while i < 6:
+    print(i)
+    i += 1  # remember to increment i, or else the loop will continue forever
+
+# Example 2: Using an index variable
+i = 0
+while i < 5:
+    print("Iteration:", i)
+    i += 1
