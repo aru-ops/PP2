7184faed794041c38a2c7f41eeadc5d10f3f54f6
aru-ops <aruzantaboldieva@gmail.com>
2026-02-04 21:42:20 +0500

    intro for boolean

---
+++ practice2/boolean/boolean_intro.py
@@ -0,0 +1,12 @@
+# Basic True/False expressions
+print(10 > 9)   # True
+print(10 == 9)  # False
+print(10 < 9)   # False
+
+# If statement based on condition
+a = 200
+b = 33
+if b > a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a")
